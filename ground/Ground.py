d5edb68edbdd9692a752482c3e8711a5f9c129e5
Jasmin <zasminalave@gmail.com>
2023-12-12 19:05:37 +1000

    delete trash

--- ground/Ground.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-
-class Ground(pygame.sprite.Sprite):
-
-    def __init__(self, img, x, y):
-        super().__init__()
-        self.image = pygame.image.load(img)
-        self.rect = self.image.get_rect(bottomleft=(x, y))
